b0ab280b64d12d9b2b4f183178e44e84803e28c0
Uhffection <142746146+Uhffection@users.noreply.github.com>
2026-05-29 22:14:08 +0000

    Create calculateBillTotal function

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+function calculateBillTotal(total) {
+    var tax = total * 0.095
+    var tip = total * 0.2
+    return total + tax + tip
+}
 
+var output = calculateBillTotal(20);
+
+console.log(output);
